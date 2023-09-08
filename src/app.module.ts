e8b6908e5e65b746305150f9094bd92947630fa5
faiqahsan786 <faiq.git@gmail.com>
2023-09-08 15:51:30 +0500

    changes moved to apso-cli

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,22 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { typeOrmAsyncConfig } from './orm.config';
 import { HealthCheckController } from './healthCheck/HealthCheckController';
-import moduleImports, { extendedScalarResolvers } from './autogen';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { join } from 'path';
+import moduleImports from './autogen';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      resolvers: extendedScalarResolvers,
-      sortSchema: true,
-      context: ({ req }) => ({ req }),
-    }),
     ...moduleImports,
   ],
   controllers: [HealthCheckController],
