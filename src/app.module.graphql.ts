55f62019050e081c6804914ecb673359dcfda665
Matthew Cullerton <matthew.cullerton@wetransfer.com>
2024-04-25 21:24:53 -0500

    fixing dormant gql issue

--- src/app.module.graphql.ts
+++ src/app.module.graphql.ts
@@ -4,7 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { typeOrmAsyncConfig } from './orm.config';
 import { HealthCheckController } from './healthCheck/HealthCheckController';
 import moduleImports from './autogen';
-import { extendedScalarResolvers } from './autogen';
+// import { extendedScalarResolvers } from './autogen';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
@@ -16,7 +16,7 @@ import { join } from 'path';
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      resolvers: extendedScalarResolvers,
+      // resolvers: extendedScalarResolvers,
       sortSchema: true,
       context: ({ req }) => ({ req }),
     }),
