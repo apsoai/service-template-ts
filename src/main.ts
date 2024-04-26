ede12c2db3dd2d1db2059572e36870b5204651f3
Matthew Cullerton <matthew.cullerton@wetransfer.com>
2024-04-25 20:57:40 -0500

    adding containers

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { AppModule } from './app.module';
+import { AppModule } from './app.module.rest';
 import { TypeORMErrorFilter } from './utils/errors/db-exception.filter';
 
 async function bootstrap() {
