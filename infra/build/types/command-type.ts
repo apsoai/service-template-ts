88cf6c2dcc6d968ae0146b726db54fbd95ef0b0e
cultron <mscullerton@gmail.com>
2025-01-20 12:52:10 -0600

    remove infra code

--- infra/build/types/command-type.ts
+++
@@ -1,7 +0,0 @@
-export enum CommandType {
-  Provision = "provision",
-  Init = "init",
-  Scaffold = "scaffold",
-  Deploy = "deploy",
-  Destroy = "destroy",
-}
--- infra/build/utils/types.ts
+++
@@ -1,8 +0,0 @@
-type JSONValue = string | number | boolean | JSONObject | JSONArray;
-
-export interface JSONObject {
-  [key: string]: JSONValue;
-}
-
-/* eslint-disable-next-line @typescript-eslint/no-empty-interface */
-export interface JSONArray extends Array<JSONValue> {}
