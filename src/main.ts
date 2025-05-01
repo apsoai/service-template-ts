3ee06e4a56899aefe28625df7afc1444e692e6da
cultron <mscullerton@gmail.com>
2025-05-01 15:46:03 -0500

    updating entrypoints to use common config and fix docs

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module.rest';
-import { TypeORMErrorFilter } from './utils/errors/db-exception.filter';
+import { configureNestApp } from './config/nest-app.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalFilters(new TypeORMErrorFilter());
-
-  const options = new DocumentBuilder()
-    .setTitle('{ApplicationName}')
-    .setVersion('1.0')
-    .build();
-  // const document = SwaggerModule.createDocument(app, options);
-  // SwaggerModule.setup('_docs', app, document);
-
+  await configureNestApp(app);
   await app.listen(process.env.APP_PORT || 3000);
 }
 
